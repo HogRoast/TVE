042fac16ea263534831806a356867aa15e4fab3d
Stuart <monkeeferret@gmail.com>
2018-08-08 18:44:37 +0100

    Added modules directory and module for TVS related consts.

---
+++ modules/tvs_exports.js
@@ -0,0 +1,5 @@
+// SHM - account id hardcoded for testing purposes
+export const accountId = "accountId_example";
+export const baseURL = "http://localhost:8080/tradingview/v1/ui/";
+export const sendOrderURL = baseURL + `/tradingview/v1/accounts/${accountId}/orders`;
+
