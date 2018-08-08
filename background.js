a18036b72115a127d421419f0c0d826d71a99fb9
Stuart <monkeeferret@gmail.com>
2018-08-08 18:48:24 +0100

    Added code to parse order message into TVS friendly POST paramters and send as HTTP request.

--- background.js
+++ background.js
@@ -1,14 +1,52 @@
 'use strict';
 
+import * as tvs from '/modules/tvs_exports.js';
+
+function createOrderParams(msg) {
+    // SHM - should the parameter validation be done here or elsewhere?
+    var params = `
+accountId=${tvs.accountId}&
+instrument=${msg.inst}&
+qty=${msg.size}&
+side=${msg.side}&
+type=${msg.type}&
+limitPrice=${msg.price}&
+${stop in msg ? `stopPrice=${msg.price + msg.stop}&` : ``}
+${tif in msg ? `durationType=${msg.tif}&` : ``}
+${goodTil in msg ? `durationDateTime=${msg.goodTil}&` : ``}
+${stop in msg ? `stopLoss=${msg.price + msg.stop}&` : ``}
+${take in msg ? `takeProfit=${msg.price + msg.take}&` : ``}
+${digSig in msg ? `digitalSignature=${msg.digSig}&` : ``}
+${id in msg? `requestId=${msg.id}&` : ``}
+`;
+    console.log(params);
+    return params;
+}
+
+function processMessage(msg) {
+    if(msg.type == "order") {
+        // SHM - send order details to TVS...
+        var request = new XMLHttpRequest();
+        request.open('POST', tvs.sendOrderURL);
+        request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        request.onload = function() {
+            console.log(this.responseText)
+        };
+        request.send(createOrderParams(msg));
+    } else {
+        console.log('Unhandled message type : ' + msg.type);
+    }
+    return;
+}
+
 chrome.runtime.onInstalled.addListener(function() {
     chrome.runtime.onConnect.addListener(function(port) {
-        console.log("Incoming connection on port: " + port.name);
-        if(port.name == "TVE-comms-port") {
-            port.postMessage({type:"connAck", payload:"Background:connection established"});
+        console.log('Incoming connection on port: ' + port.name);
+        if(port.name == 'TVE-comms-port') {
+            port.postMessage({type:'connAck', payload:'Background:connection established'});
             port.onMessage.addListener(function(msg) {
                 console.log(msg);            
-                // SHM - send order details to TVS here...
-
+                processMessage(msg);
             });
         }
     });
@@ -18,7 +56,7 @@ chrome.runtime.onInstalled.addListener(function() {
             conditions: [new chrome.declarativeContent.PageStateMatcher({
                 pageUrl: {  hostEquals: 'uk.tradingview.com',
                             schemes: ['https']},
-                css: ["div[class='layout__area--right']"]
+                css: ['div[class='layout__area--right']']
             })],
             actions: [
                 new chrome.declarativeContent.ShowPageAction(),
